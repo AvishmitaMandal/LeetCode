f5a992f94912b24baee0e15810daf44dd2c304b2
AvishmitaMandal <146691031+AvishmitaMandal@users.noreply.github.com>
2024-07-25 12:12:57 +0530

    Time: 78 ms (72.02%), Space: 27.2 MB (38.7%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -6,17 +6,34 @@
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        hashmap = {}
+        lenA, lenB = 0, 0
         nodeA, nodeB = headA, headB
 
         while nodeA:
-            hashmap[nodeA] = 1
+            lenA += 1
             nodeA = nodeA.next
 
         while nodeB:
-            if nodeB in hashmap:
-                return nodeB
+            lenB += 1
             nodeB = nodeB.next
 
-        return None
+        if lenA > lenB:
+            diff = lenA - lenB
+            nodeA, nodeB = headA, headB
+            while diff:
+                nodeA = nodeA.next
+                diff -= 1
+
+        else:
+            diff = lenB - lenA
+            nodeA, nodeB = headA, headB
+            while diff:
+                nodeB = nodeB.next
+                diff -= 1
+
+        while nodeA and nodeB and nodeA != nodeB:
+            nodeA = nodeA.next
+            nodeB = nodeB.next
+
+        return nodeA
         
