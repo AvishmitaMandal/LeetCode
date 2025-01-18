273bbc7ab86d547c154720a4a27949cb9a6032e1
AvishmitaMandal <146691031+AvishmitaMandal@users.noreply.github.com>
2025-01-18 10:48:49 -0500

    Time: 19 ms (56.44%), Space: 18.1 MB (7.11%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,29 +1,35 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if len(s) < 2:
-            return len(s)
-        
-        start, end, max_len = 0,0,0
-        d = {}
+        '''
+        abcabcbb
+        pwwkew
+        '''
+        if len(s) == 0:
+            return 0
+            
+        max_length = 1
+        start, end = 0, 0
+        hash_map = {}
+        hash_map[s[start]] = 1
 
-        while end < len(s):
-
-            # No duplicates in the substring
-            if s[end] not in d or d[s[end]]==0:
-                d[s[end]] = 1
-
-            # Duplicate found
-            else:
-                while s[start] != s[end]:
-                    d[s[start]] = 0
-                    start+=1
-                d[s[start]] = 0
-                start += 1
-                d[s[end]] = 1
-
-            max_len = max(max_len, end-start+ 1)
+        while end < len(s)-1:
             end += 1
 
-        return max_len
+            if s[end] not in hash_map or hash_map[s[end]] == 0:
+                hash_map[s[end]] = 1
+                length = end - start + 1
+                if length > max_length:
+                    max_length = length
+            else :
+                hash_map[s[end]] += 1
+                while start <= end and s[start] != s[end]:
+                    hash_map[s[start]] -= 1
+                    start += 1
+                hash_map[s[start]] -= 1
+                start += 1
 
+        return max_length
+                    
+
+            
         
