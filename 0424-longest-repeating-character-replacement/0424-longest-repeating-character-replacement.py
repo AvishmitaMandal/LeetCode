352191efcd3e5afb01ed1c6ea8c548d3c30ceda7
AvishmitaMandal <146691031+AvishmitaMandal@users.noreply.github.com>
2025-01-18 12:40:37 -0500

    Time: 107 ms (85.53%), Space: 18.1 MB (12.38%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,34 +1,40 @@
+import heapq
+
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        '''
+            ABCAAABCAABAC, k = 3
+        '''
         if len(s) == 0:
             return 0
-        
-        freq_dict = {}
 
         start, end = 0, 0
-        most_freq = 0
-
-        res = 0
+        maxLength = 1
+        hashmap = {}
+        highFreqChar, highFreqVal = '', 0
 
         while end < len(s):
-            if s[end] in freq_dict:
-                freq_dict[s[end]] += 1
+            if s[end] not in hashmap or hashmap[s[end]] == 0:
+                hashmap[s[end]] = 1
             else:
-                freq_dict[s[end]] = 1
+                hashmap[s[end]] += 1
 
-            most_freq = max(most_freq, freq_dict[s[end]])
-
-            rep_freq = (end - start + 1) - most_freq
-            
-            if rep_freq > k:
-                freq_dict[s[start]] -= 1
+            if hashmap[s[end]] > highFreqVal:
+                highFreqChar = s[end]
+                highFreqVal = hashmap[s[end]]
+                
+            length = end-start+1
+            if length - highFreqVal <= k:
+                maxLength = max(maxLength, length)
+            else:
+                hashmap[s[start]] -= 1
                 start += 1
-            else:
-                res = max(res, end - start + 1)
 
             end += 1
 
-        return res
+        return maxLength
 
+            
 
         
+
