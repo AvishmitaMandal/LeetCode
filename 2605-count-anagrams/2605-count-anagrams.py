342092a8c22b07511a59f559de325588e5bbd85e
AvishmitaMandal <146691031+AvishmitaMandal@users.noreply.github.com>
2025-01-24 10:02:45 -0500

    Time: 2043 ms (5.27%), Space: 19.4 MB (41.93%) - LeetHub

---
+++ 2605-count-anagrams/2605-count-anagrams.py
@@ -0,0 +1,30 @@
+from collections import defaultdict
+import math
+
+class Solution:
+    def countAnagrams(self, s: str) -> int:
+        '''
+        "too hot for me"
+        '''
+        MOD = 10**9 + 7
+
+        i = 0
+        res = 1
+        while i < len(s):
+            hashmap = defaultdict(int)
+            word_len = 0
+            while i < len(s) and s[i]!= ' ':
+                hashmap[s[i]]+= 1
+                word_len += 1
+                i += 1
+
+            res *= math.factorial(word_len)
+            for key, val in hashmap.items():
+                res //= math.factorial(val)
+                res = res
+            i += 1
+
+        return res % MOD
+            
+
+        
