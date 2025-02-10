6c4953cb398432097a3e716b96f5091e9cf8ba34
AvishmitaMandal <146691031+AvishmitaMandal@users.noreply.github.com>
2025-02-10 12:58:35 -0500

    Time: 0 ms (100%), Space: 17.7 MB (65.42%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -2,40 +2,29 @@ class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
+        nums1 = [1,2,3,0,0,0], nums2 = [2,5,6]
         """
 
-        nums = []
-        for x in range(m):
-            nums.append(nums1[x])
-
-        it1, it2, x = 0, 0, 0
-        while it1 < m and it2 < n:
-            if it1 == m or it2 == n:
-                break
-
-            if nums[it1] <= nums2[it2]:
-                nums1[x] = nums[it1]
-                it1 += 1
-
+        temp = []
+        x, y = 0, 0
+        while x < m and y < len(nums2):
+            if nums1[x] <= nums2[y] :
+                temp.append(nums1[x])
+                x += 1
             else:
-                nums1[x] = nums2[it2]
-                it2 += 1
+                temp.append(nums2[y])
+                y += 1
 
+        while x < m:
+            temp.append(nums1[x])
             x += 1
 
-        i, j = it1, it2
+        while y < len(nums2):
+            temp.append(nums2[y])
+            y += 1
 
-        if it1 == m:
-            while j < n and x < m+n:
-                nums1[x] = nums2[j]
-                j += 1
-                x += 1
+        print(temp)
 
-        elif it2 == n:
-            while i < m and x < m+n:
-                nums1[x] = nums[i]
-                i += 1
-                x += 1
-
-        return nums1
+        for x in range(len(temp)):
+            nums1[x] = temp[x]
         
