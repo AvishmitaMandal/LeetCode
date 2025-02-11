e7be7bcdbb87f1b8497eda47ddf5ca0a6353d116
AvishmitaMandal <146691031+AvishmitaMandal@users.noreply.github.com>
2025-02-11 09:05:38 -0500

    Time: 30 ms (21.01%), Space: 17.8 MB (76.86%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,35 +1,30 @@
+from collections import defaultdict
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         '''
         abcabcbb
-        pwwkew
+        tmmzuxt
         '''
-        if len(s) == 0:
+        n = len(s)
+        if n == 0:
             return 0
-            
-        max_length = 1
-        start, end = 0, 0
-        hash_map = {}
-        hash_map[s[start]] = 1
 
-        while end < len(s)-1:
-            end += 1
+        start, end = 0,0
+        mp = defaultdict(int)
 
-            if s[end] not in hash_map or hash_map[s[end]] == 0:
-                hash_map[s[end]] = 1
-                length = end - start + 1
-                if length > max_length:
-                    max_length = length
-            else :
-                hash_map[s[end]] += 1
-                while start <= end and s[start] != s[end]:
-                    hash_map[s[start]] -= 1
+        res = 0
+        while end < n:
+            if s[end] in mp and mp[s[end]] != 0:
+                while s[start] != s[end]:
+                    mp[s[start]] -= 1
                     start += 1
-                hash_map[s[start]] -= 1
+                mp[s[start]] -= 1
                 start += 1
 
-        return max_length
-                    
+            res = max(res, end-start+1)
+            mp[s[end]] += 1
+            end += 1
 
-            
+        return res
+
         
