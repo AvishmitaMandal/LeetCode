1bf34303d392fb342cf08cbb4aecfc031bdf6c5a
AvishmitaMandal <146691031+AvishmitaMandal@users.noreply.github.com>
2025-02-14 11:55:20 -0500

    Time: 459 ms (7.02%), Space: 78.7 MB (13.48%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,36 +1,69 @@
-from collections import OrderedDict
+class Node(object):
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.next = None
+        self.prev = None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.dict = OrderedDict()
         self.capacity = capacity
+        self.item_count = 0
+        self.mp = {}
         
+        self.head = Node(-1, -1)
+        self.tail = Node(-1, -1)
+
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def remove(self, node):
+        node_prev = node.prev
+        node_next = node.next
+        node_prev.next = node_next
+        node_next.prev = node_prev
+        node.next = None 
+        node.prev = None
+
+        return node
+
+    def insert_beginning(self, node):
+        node_next = self.head.next
+        self.head.next = node
+        node.prev = self.head
+        node.next = node_next
+        node_next.prev = node
 
     def get(self, key: int) -> int:
-        if key in self.dict:
-            val = self.dict.pop(key)
-            self.dict[key] = val
-            return val
-        
+        if key in self.mp:
+            node = self.mp[key]
+            node = self.remove(node)
+            self.insert_beginning(node)
+            return node.val
         return -1
-
-    def put(self, key: int, value: int) -> None:
-        if key in self.dict:
-            val = self.dict.pop(key)
-            self.dict[key] = value
-
-        elif len(self.dict) == self.capacity:
-            # remove the first item
-            self.dict.popitem(last = False)
-            # and then insert key-value
-            self.dict[key] = value
-
-        else:
-            # insert the key-value
-            self.dict[key] = value
         
+    def put(self, key: int, value: int) -> None:
+        # check in map - if present ? -> update, remove, insert in beginning
+        if key in self.mp:
+            node = self.mp[key]
+            node.val = value
+            node = self.remove(node)
+            self.insert_beginning(node)
+        # if not present - check capacity overflow ? -> evict last node, insert begining 
+        elif self.item_count == self.capacity:
+            node = self.remove(self.tail.prev)
+            del self.mp[node.key]
 
+            new_node = Node(key, value)
+            self.insert_beginning(new_node)
+            self.mp[key] = new_node
+        # if capacity -> insert beginning , update capacity
+        else:
+            new_node = Node(key, value)
+            self.insert_beginning(new_node)
+            self.item_count += 1
+            self.mp[key] = new_node
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
