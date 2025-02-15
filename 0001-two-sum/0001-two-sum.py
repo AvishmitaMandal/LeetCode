7b538bd5bb095fb75b87d5f73b378bab303406de
AvishmitaMandal <146691031+AvishmitaMandal@users.noreply.github.com>
2025-02-14 22:24:46 -0500

    Time: 0 ms (100%), Space: 18.9 MB (49.86%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,35 +1,15 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        mp = {}
         res = []
 
-        # Brute force way
-        # [2,7,11,15] - O(n^2) time and O(1) space 
-
-        # Maps
-        # [2,7,11,15]
-
-        hashMap = {}
         for x in range(len(nums)):
-            if nums[x] not in hashMap:
-                hashMap[nums[x]] = []
-            hashMap[nums[x]].append(x)
-        print(hashMap)
+            if target - nums[x] in mp:
+                res.append(x)
+                res.append(mp[target-nums[x]])
+                return res
+            mp[nums[x]] = x
+            
 
-        for key, val in hashMap.items():
-            if (target - key) in hashMap:
-                if target - key != key:
-                    res.append(val[0])
-                    res.append(hashMap[target-key][0])
-                    return res
-                else:
-                    if len(hashMap[target-key]) > 1:
-                        res.append(hashMap[target-key][0])
-                        res.append(hashMap[target-key][1])
-                        return res
-
-        return 0
-
-        
-
-
+        return res
         
