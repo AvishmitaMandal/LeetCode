ddb3468e43b281b4cf9d74e8d26303c79369d4dd
AvishmitaMandal <146691031+AvishmitaMandal@users.noreply.github.com>
2025-02-23 12:59:14 -0500

    Time: 14 ms (92.61%), Space: 18 MB (87.99%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,40 +1,31 @@
+from collections import deque
 class Solution:
-    def bfs(self, x, y, grid):
-        m = len(grid)
-        n = len(grid[0])
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        max_area = 0
+        directions = [[0,1],[0,-1],[1,0],[-1,0]]
 
         q = deque()
-        q.append((x,y))
-        area = 0
-        directions = [(1,0), (0,1), (-1,0), (0,-1)]
 
-        while q:
-            cord = q.popleft()
-            print(type(cord))
-            print(cord)
-            x, y = cord
-            area += 1
-            for dx, dy in directions:
-                new_x, new_y = x+dx, y+dy
-                if 0 <= new_x < m and 0 <= new_y < n and grid[new_x][new_y] == 1:
-                    grid[new_x][new_y] = 0
-                    q.append((new_x, new_y))
+        for y in range(m):
+            for x in range(n):
+                if grid[y][x] == 1:
+                    q.append((x,y))
+                    grid[y][x] = 0
+                    count = 0
+                    while q:
+                        (curr_x, curr_y) = q.popleft()
+                        count += 1
+                        for dx, dy in directions:
+                            new_x, new_y = curr_x + dx, curr_y + dy
+                            if new_x >= 0 and new_y >= 0 and new_x < n and new_y < m and grid[new_y][new_x] == 1:
+                                grid[new_y][new_x] = 0
+                                q.append((new_x, new_y))
+                    max_area = max(max_area, count)
 
-        return area
-
-
-
-    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        m = len(grid)
-        n = len(grid[0])
-        max_area = 0
-
-        for x in range(m):
-            for y in range(n):
-                if grid[x][y] == 1:
-                    grid[x][y] = 0
-                    area = self.bfs(x,y,grid)
-                    max_area = max(max_area, area)
 
         return max_area
+                        
+
+
         
