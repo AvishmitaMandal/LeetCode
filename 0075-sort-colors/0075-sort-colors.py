835878075162646ecccbc8ba7415fe8118dc6d8c
AvishmitaMandal <146691031+AvishmitaMandal@users.noreply.github.com>
2025-04-10 22:16:42 -0400

    Time: 0 ms (100%), Space: 17.8 MB (33.69%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,38 +3,28 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        dict_map = {}
-
+        zeroes, ones, twos = 0, 0, 0
         for num in nums:
-            if num in dict_map:
-                dict_map[num] += 1
-            else:
-                dict_map[num] = 1
-        
-        zeroes, ones, twos = 0,0,0
-        
-        if 0 in dict_map:
-            zeroes = dict_map[0]
-        if 1 in dict_map:
-            ones = dict_map[1]
-        if 2 in dict_map:
-            twos = dict_map[2]
+            if num == 0:
+                zeroes += 1
+            if num == 1:
+                ones += 1
+            if num == 2:
+                twos += 1
 
         x = 0
-        while zeroes != 0:
+        while zeroes:
             nums[x] = 0
-            x += 1
             zeroes -= 1
-
-        while ones != 0:
+            x += 1
+    
+        while ones:
             nums[x] = 1
-            x += 1
             ones -= 1
-
-        while twos != 0:
-            nums[x] = 2
             x += 1
+
+        while twos:
+            nums[x] = 2
             twos -= 1
-
-
+            x += 1
         
