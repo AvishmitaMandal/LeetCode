ea8379613b55dd8566a011ca070f4a305001a8be
AvishmitaMandal <146691031+AvishmitaMandal@users.noreply.github.com>
2025-04-17 22:35:17 -0400

    Time: 31 ms (70.71%), Space: 18 MB (80.89%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,4 +1,21 @@
 class Solution:
+    def binary_search(self, seq, target):
+        ans = len(seq)
+        low, high = 0, len(seq)-1
+
+        while low <= high:
+            mid = (low+high)//2
+
+            if seq[mid] >= target:
+                ans = min(ans,mid)
+                high = mid-1
+            else:
+                low = mid + 1
+
+        seq[ans] = target
+        return seq
+         
+
     def lengthOfLIS(self, nums: List[int]) -> int:
         seq = []
         n = len(nums)
@@ -12,7 +29,8 @@ class Solution:
                 for y in range(len(seq)):
                     if seq[y] >= nums[x]:
                         seq[y] = nums[x]
-                        break
+                        break 
+                seq = self.binary_search(seq, nums[x])
 
         return len(seq)
             
