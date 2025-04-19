d13251d9766d71f692204c23f4dbf725fc8572e2
AvishmitaMandal <146691031+AvishmitaMandal@users.noreply.github.com>
2025-04-19 09:42:53 -0400

    Time: 0 ms (100%), Space: 17.6 MB (84.1%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
@@ -7,42 +7,27 @@ class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if head == None:
             return None
-        odd = head
-        
+        f = head
+
         if head.next == None:
             return head
-        even = head.next
+        s = head.next
+        new_head = s
 
-        list1, list2 = odd, even
-        while True:
-            if even.next == None:
-                odd.next = None
+        p = None
+        n = s.next
+        while s:
+            s.next = f
+            f.next = n
+            if p != None:
+                p.next = s
+            if n == None or n.next == None:
                 break
-            odd_next = even.next
-            odd.next = odd_next
-            odd = odd_next
-
-            if odd_next.next == None:
-                even.next = None
-                break
-            even_next = odd_next.next
-            even.next = even_next
-            even = even_next
-
-        # print(list1)
-        # print(list2)
-
-        even, odd = list2, list1
-        while even and odd:
-            even_next = even.next
-            odd_next = odd.next
-            even.next = odd
-            if even_next == None:
-                break
-            odd.next = even_next
-            even = even_next
-            odd = odd_next
+            p = f
+            f = n
+            s = n.next
+            n = s.next
         
-        return list2
+        return new_head
 
         
